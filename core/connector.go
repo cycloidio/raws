8d642f61b3fe12a2f3f6137fca49a28ca3e91207
Hugo Rosnet <hugo.rosnet@cycloid.io>
2017-06-12 15:42:02 +0200

    connector: add comment for doc and move const
    The comments have been added in case we want to generate documentation
    via godoc, like this is the case on many other public projects.
    
    The const variable didn't need to be accessed by all the methods so
    moving it to the function using it was enough.

--- core/connector.go
+++ core/connector.go
@@ -15,6 +15,12 @@ import (
 	"github.com/pkg/errors"
 )
 
+// The Connector provides easy access to AWS SDK calls.
+//
+// By using it, calls can be made directly through multiple regions, and will filter only data that belongs to you.
+// For example, when fetching the list of AMI, or snapshots.
+//
+// In order to start making calls, only calling NewConnector is required.
 type Connector struct {
 	regions   []string
 	svcs      []*serviceConnector
@@ -22,6 +28,18 @@ type Connector struct {
 	accountID *string
 }
 
+// NewConnector returns an object which also contains the accountID and extend the different regions to use.
+//
+// The accountID is helpful to return only the AMI or snapshots that belong to the account.
+//
+// While the regions slice also supports regex so, "eu-*" can be passed, and will be extended to: eu-west-1, eu-west-2 &
+// eu-central-1.
+//
+// When calls are done through the Connector instance, then all regions will be called for those services.
+// Thus making requests to AWS much easier than through the different connectors/regions of its go SDK.
+//
+// The connections are not all established while instancing, but the various sessions are, this way connections are only
+// made for services that are called, otherwise only the sessions remain.
 func NewConnector(accessKey string, secretKey string, regions []string, config *aws.Config) (*Connector, error) {
 	/* The default region is only used to (1) get the list of region and
 	 * (2) get the account ID associated with the credentials.
@@ -30,7 +48,7 @@ func NewConnector(accessKey string, secretKey string, regions []string, config *
 	 * region is specified when instantiating the connector, then it will
 	 * not try to establish any connections with AWS services.
 	 */
-	var defaultRegion string = "eu-west-1"
+	const defaultRegion string = "eu-west-1"
 	var defaultSession *session.Session
 	var c Connector = Connector{}
 
