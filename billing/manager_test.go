ecf50c5791382a32643b7d96c9499530c9ee9b88
Hugo Rosnet <hugo.rosnet@cycloid.io>
2017-08-11 15:36:25 +0200

    manager: add test to the manager Import method
    The Import method is the one doing calls to all the other components:
    checker, downloader, loader, injector.

--- billing/manager_test.go
+++ billing/manager_test.go
@@ -1,11 +1,111 @@
 package billing
 
 import (
+	"errors"
 	"testing"
 )
 
 func TestBillingManager_Import(t *testing.T) {
+	const (
+		date   = "2017-01"
+		bucket = "test-bucket"
+	)
+	var awsReader mockAWSReader = mockAWSReader{
+		accountID: "111111111111",
+	}
 
+	tests := []struct {
+		name             string
+		mockedChecker    mockChecker
+		mockedDownloader mockDownloader
+		mockedLoader     mockLoader
+		mockedInjector   mockInjector
+		expectedError    error
+	}{
+		{name: "no errors during any calls",
+			mockedChecker: mockChecker{
+				cb: true,
+				ce: nil,
+			},
+			mockedDownloader: mockDownloader{
+				ds: "",
+				de: nil,
+				us: "",
+				ue: nil,
+			},
+			mockedInjector: mockInjector{
+				crde: nil,
+				crpe: nil,
+			},
+			mockedLoader:  mockLoader{},
+			expectedError: nil,
+		},
+		{name: "errors during Check",
+			mockedChecker: mockChecker{
+				cb: true,
+				ce: errors.New("error during check"),
+			},
+			expectedError: errors.New("error during check"),
+		},
+		{name: "errors during Download",
+			mockedChecker: mockChecker{
+				cb: true,
+				ce: nil,
+			},
+			mockedDownloader: mockDownloader{
+				ds: "",
+				de: nil,
+				us: "",
+				ue: errors.New("error during download"),
+			},
+			expectedError: errors.New("error during download"),
+		},
+		{name: "errors during Unzip",
+			mockedChecker: mockChecker{
+				cb: true,
+				ce: nil,
+			},
+			mockedDownloader: mockDownloader{
+				ds: "",
+				de: nil,
+				us: "",
+				ue: errors.New("error during unzip"),
+			},
+			expectedError: errors.New("error during unzip"),
+		},
+		{name: "errors during Inject",
+			mockedChecker: mockChecker{
+				cb: true,
+				ce: nil,
+			},
+			mockedDownloader: mockDownloader{
+				ds: "",
+				de: nil,
+				us: "",
+				ue: nil,
+			},
+			mockedInjector: mockInjector{
+				crde: nil,
+				crpe: errors.New("error during CreateReport"),
+			},
+			mockedLoader:  mockLoader{},
+			expectedError: errors.New("error during CreateReport"),
+		},
+	}
+
+	for i, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := &BillingManager{
+				s3Connector: awsReader,
+				checker:     tt.mockedChecker,
+				downloader:  tt.mockedDownloader,
+				loader:      tt.mockedLoader,
+				injector:    tt.mockedInjector,
+			}
+			err := m.Import(date, bucket)
+			checkErrors(t, tt.name, i, err, tt.expectedError)
+		})
+	}
 }
 
 func TestBillingManager_getS3Filename(t *testing.T) {
