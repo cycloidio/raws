9285472c1ab05e484d8479b4dd477a31c734096f
Ivan Fraixedes <ivan.fraixedes@cycloid.io>
2018-02-06 16:58:30 +0000

    helper_test: Mark the helper func
    Mark the test helper function as helper for having error reporting in
    the line and test which fails, not indicating the helper function file
    and line.

--- helpers_test.go
+++ helpers_test.go
@@ -6,6 +6,8 @@ import (
 )
 
 func checkErrors(t *testing.T, name string, index int, err error, expected error) {
+	t.Helper()
+
 	if err != nil && !reflect.DeepEqual(err, expected) {
 		t.Errorf("%s [%d] - errors: received=%+v | expected=%+v",
 			name, index, err.Error(), expected.Error())
