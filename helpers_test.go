d558fa6ffb702b22d4dd73faeebdd09bcc41ef73
Ivan Fraixedes <ivan.fraixedes@cycloid.io>
2018-03-12 16:47:20 +0000

    helpers_test: Add new func to check error without iteration
    Add a new test helper function to check the errors but without having to
    provide a name and iteration number, in order to write test which aren't
    written in table format.

--- helpers_test.go
+++ helpers_test.go
@@ -9,11 +9,25 @@ func checkErrors(t *testing.T, name string, index int, err error, expected error
 	t.Helper()
 
 	if err == nil && expected != nil {
-		t.Errorf("%s [%d] - errors: received=nil | expected=%v", name, index, expected)
+		if index >= 0 {
+			t.Errorf("%s [%d] - errors: received=nil | expected=%v", name, index, expected)
+		} else {
+			t.Errorf("%s - errors: received=nil | expected=%v", name, expected)
+		}
+
 		return
 	}
 
 	if !reflect.DeepEqual(err, expected) {
-		t.Errorf("%s [%d] - errors: received=%+v | expected=%+v", name, index, err, expected)
+		if index >= 0 {
+			t.Errorf("%s [%d] - errors: received=%+v | expected=%+v", name, index, err, expected)
+		} else {
+			t.Errorf("%s - errors: received=%+v | expected=%+v", name, index, expected)
+		}
 	}
+}
+
+func checkError(t *testing.T, err error, expected error) {
+	t.Helper()
+	checkErrors(t, t.Name(), -1, err, expected)
 }
